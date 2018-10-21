fe3fdff879560ccf9f3e20b14b7d814476f49690
pouyaghazvini <pouyaghazvini@gmail.com>
2018-10-21 14:39:44 -0700

    added configuration object for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,18 @@ module.exports = {
     filename: 'bundle.js'
   }
 };
+
+module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+}
