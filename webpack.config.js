f2a6f850a5830d57f847d4905a5e3b46ae1b4394
pouyaghazvini <pouyaghazvini@gmail.com>
2018-10-21 14:56:27 -0700

    finished development setup and created a client folder

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module: {
       use: {
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          
         }
       }
     }
